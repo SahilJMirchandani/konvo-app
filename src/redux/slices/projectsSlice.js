e2b06ce6afdf8505650ee0913220e0c3bc717924
Sahil Mirchandani <sahilmirchandani@gmail.com>
2026-05-28 23:47:19 +0530

    Update projectsSlice.js

--- src/redux/slices/projectsSlice.js
+++ src/redux/slices/projectsSlice.js
@@ -6,7 +6,7 @@ export const fetchProjects = createAsyncThunk('projects/fetchAll', async (_, { r
     const res = await API.get('/projects');
     return res.data;
   } catch (err) {
-    return rejectWithValue(err.response.data);
+    return rejectWithValue(err.response?.data);
   }
 });
 
@@ -15,7 +15,7 @@ export const createProject = createAsyncThunk('projects/create', async (data, { 
     const res = await API.post('/projects', data);
     return res.data;
   } catch (err) {
-    return rejectWithValue(err.response.data);
+    return rejectWithValue(err.response?.data);
   }
 });
 
@@ -24,20 +24,30 @@ export const deleteProject = createAsyncThunk('projects/delete', async (id, { re
     await API.delete(`/projects/${id}`);
     return id;
   } catch (err) {
-    return rejectWithValue(err.response.data);
+    return rejectWithValue(err.response?.data);
   }
 });
 
 const projectsSlice = createSlice({
   name: 'projects',
-  initialState: { list: [], loading: false, error: null },
+  initialState: { list: [], loading: false, creating: false, error: null },
   reducers: {},
   extraReducers: (builder) => {
     builder
       .addCase(fetchProjects.pending, (state) => { state.loading = true; })
       .addCase(fetchProjects.fulfilled, (state, action) => { state.loading = false; state.list = action.payload; })
       .addCase(fetchProjects.rejected, (state, action) => { state.loading = false; state.error = action.payload; })
-      .addCase(createProject.fulfilled, (state, action) => { state.list.push(action.payload); })
+
+      .addCase(createProject.pending, (state) => { state.creating = true; })
+      .addCase(createProject.fulfilled, (state, action) => { 
+        state.creating = false; 
+        state.list.push(action.payload); 
+      })
+      .addCase(createProject.rejected, (state, action) => { 
+        state.creating = false; 
+        state.error = action.payload; 
+      })
+
       .addCase(deleteProject.fulfilled, (state, action) => {
         state.list = state.list.filter(p => p.id !== action.payload);
       });
