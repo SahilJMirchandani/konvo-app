1f54165a568ebcd7566afbe8e7f2e1036165206e
Sahil Mirchandani <sahilmirchandani@gmail.com>
2026-05-28 17:57:02 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,4 @@
 import 'react-native-gesture-handler';
-import { enableScreens } from 'react-native-screens';
-enableScreens();
-
 import { registerRootComponent } from 'expo';
 import App from './App';
 
