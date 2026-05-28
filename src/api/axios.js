6c434fa3bc401b87167dbdf4a309c4ce073184ee
Sahil Mirchandani <sahilmirchandani@gmail.com>
2026-05-28 23:53:25 +0530

    Update axios.js

--- src/api/axios.js
+++ src/api/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const API = axios.create({
-  baseURL: 'https://konvo-backend-jv18.onrender.com/api',
+  baseURL: 'https://konvo-backend-jv18.onrender.com',
 });
 
 API.interceptors.request.use(async (config) => {
